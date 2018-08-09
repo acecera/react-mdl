781e07f22ed54b8255cce3e26281337591f70442
Anthony Cera <ace_cera@yahoo.com>
2018-08-08 22:55:59 -0700

    changed app.jsx back to app.js.

---
+++ portfolio/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Navigation, Header, Drawer, Content} from "react-mdl";
+import Routes from './Components/routes';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+          <Layout>
+              <Header className="header-color" title="Anthony Cera" scroll>
+                  <Navigation>
+                      <Link to="/about">About</Link>
+                      <Link to="/skills">Skills</Link>
+                      <Link to="/portfolio">Portfolio</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Anthony Cera">
+                  <Navigation>
+                      <Link to="/about">About</Link>
+                      <Link to="/skills">Skills</Link>
+                      <Link to="/portfolio">Portfolio</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Routes />
+              </Content>
+          </Layout>
+      </div>
+
+    );
+  }
+}
+
+export default App;
