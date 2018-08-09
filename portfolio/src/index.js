47a892f36b5b331ec4992a30915adeff487921a3
Anthony Cera <ace_cera@yahoo.com>
2018-08-08 22:57:14 -0700

    added some more links for font awesome and google fonts.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
