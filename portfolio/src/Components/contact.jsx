eee7c0e1bf1c6bf4ec7754c4fff1ef8ffa2b591b
Anthony Cera <ace_cera@yahoo.com>
2018-08-14 20:40:23 -0700

    added grid and cell to layout contact page. import profile image, grid and cell.

--- portfolio/src/Components/contact.jsx
+++ portfolio/src/Components/contact.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import ProfileImage from "../images/AnthonyC.jpg";
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h1>Contact</h1>
+            <div className="contact-body">
+                <Grid>
+                    <Cell col={6}>
+                        <h2>Anthony Cera</h2>
+                        <ProfileImage />
+                    </Cell>
+                    <Cell col={6}></Cell>
+                </Grid>
             </div>
         )
     }
