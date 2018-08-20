01bf753a409fd052d8dbc67a542423043137324b
Anthony Cera <ace_cera@yahoo.com>
2018-08-20 01:44:53 -0700

    created a contact folder for the contact jsx file and css file.

---
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import ProfileImage from "../../images/Avatar.png";
+import './Contact.css';
+
+class Contact extends Component {
+    handleClick = (e) => {
+        e.preventDefault();
+        console.log('The link was clicked');
+    };
+
+    render() {
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Anthony Cera</h2>
+                        <img src={ProfileImage} alt="profile pic" style={{height: '250px'}}/>
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '2em', fontFamily: 'Oswald'}}>
+                            Please contact at anytime if you have questions or inquiries and feel free to check out my LinkedIn or GitHub!
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '32px', fontFamily: "Oswald"}}>
+                                        <i className="fas fa-mobile"/>
+                                        (818) 383-8917
+                                    </ListItemContent>
+                                </ListItem>
+
+                                 <ListItem>
+                                    <ListItemContent style={{fontSize: '32px', fontFamily: "Oswald"}}>
+                                        <i className="far fa-envelope" aria-hidden="true" />
+                                        coordinatorace@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+
+                                 <ListItem>
+                                    <ListItemContent style={{fontSize: '32px', fontFamily: "Oswald"}}>
+                                        <a href="https://www.linkedin.com/in/anthony-cera-151b9440/" rel="noopener noreferrer" target="_blank">
+                                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                        </a>
+                                        LinkedIn
+                                    </ListItemContent>
+                                </ListItem>
+
+                                 <ListItem>
+                                    <ListItemContent style={{fontSize: '32px', fontFamily: "Oswald"}}>
+                                        <a href="https://github.com/acecera" rel="noopener noreferrer" target="_blank">
+                                            <i className="fa fa-github-square" aria-hidden="true" />
+                                        </a>
+                                        GitHub
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Contact;
