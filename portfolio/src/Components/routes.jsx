a04a3b7c307509a4a8c35bb926c0ae720ed1bbd2
Anthony Cera <ace_cera@yahoo.com>
2018-08-20 01:50:57 -0700

    modified route path for the page components.

--- portfolio/src/Components/routes.jsx
+++ portfolio/src/Components/routes.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import LandingPage from "./landingPage";
-import About from "./about";
-import Skills from "./skills";
-import Portfolio from "./portfolio";
-import Contact from "./contact";
+import About from "./About/About";
+import Portfolio from "./Portfolio/Portfolio";
+import Contact from "./Contact/Contact";
 import { Switch, Route } from "react-router-dom";
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/about" component={About} />
-        <Route path="/skills" component={Skills} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
     </Switch>
